5d019d00be1288f84d6cfb91e69bb77954889fef
rayhan108 <raihanshorker56@gmail.com>
2025-12-02 10:03:25 +0600

    remove comment

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,12 +1,12 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// console.log("Firebase API Key: ", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
-// console.log("Firebase Auth Domain: ", process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
-// console.log("Firebase Project ID: ", process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
-// console.log("Firebase Storage Bucket: ", process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET);
-// console.log("Firebase Messaging Sender ID: ", process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID);
-// console.log("Firebase App ID: ", process.env.NEXT_PUBLIC_FIREBASE_APP_ID);
+console.log("Firebase API Key: ", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+console.log("Firebase Auth Domain: ", process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
+console.log("Firebase Project ID: ", process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
+console.log("Firebase Storage Bucket: ", process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET);
+console.log("Firebase Messaging Sender ID: ", process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID);
+console.log("Firebase App ID: ", process.env.NEXT_PUBLIC_FIREBASE_APP_ID);
 
 const firebaseConfig = {
   apiKey:process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
